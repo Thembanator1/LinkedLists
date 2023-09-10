cdb40d558bc9f39ff6adf5faaef81e35dfb5fa2f
Cwenga <gcolotelacwenga@gmail.com>
2023-09-10 03:08:59 +0200

    deleted:    i.html 	deleted:    i.js 	modified:   index.html 	modified:   index.js 	new file:   script.html 	new file:   script.js

--- index.js
+++ index.js
@@ -1,56 +1,65 @@
-// script.js
+// i.js
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
-// Array to store nodes and their positions
 const nodes = [];
+const links = [];
 
-// Array to store links between nodes
-let links = [];
-
-// Variable to keep track of the selected node
 let selectedNode = null;
 
-// Event listener for the "Add Node" button
-document.getElementById("addNodeButton").addEventListener("click", addNode);
+function addRandomNode() {
+    const nodeName = document.getElementById("nodeName").value;
+    if (!nodeName) return;
 
-// Event listener for the "Link Nodes" button
-document.getElementById("linkNodesButton").addEventListener("click", linkNodes);
+    // Check if a node with the same name already exists
+    if (nodes.some(node => node.name === nodeName)) {
+        alert("A node with the same name already exists.");
+        return;
+    }
 
-// Event listener for the "Delete Node" button
-document.getElementById("deleteNodeButton").addEventListener("click", deleteSelectedNode);
-
-// Event listener for canvas click to select/deselect nodes
-canvas.addEventListener("click", selectNode);
-
-// Function to add a new node
-function addNode() {
-    const x = Math.random() * canvas.width;
-    const y = Math.random() * canvas.height;
-    const nodeName1 = document.getElementById("nodeName1").value;
-    const nodeName2 = document.getElementById("nodeName2").value;
-
-    nodes.push({ x, y, nodeName1, nodeName2 });
-    drawNodes();
+    const node = { x: getRandomInt(20, canvas.width - 20), y: getRandomInt(20, canvas.height - 20), name: nodeName };
+    nodes.push(node);
+    draw();
 }
 
-// Function to link nodes
-function linkNodes() {
-    const nodeName1 = document.getElementById("nodeName1").value;
-    const nodeName2 = document.getElementById("nodeName2").value;
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-    const node1 = nodes.find(node => node.nodeName1 === nodeName1 || node.nodeName2 === nodeName1);
-    const node2 = nodes.find(node => node.nodeName1 === nodeName2 || node.nodeName2 === nodeName2);
+function addLink() {
+    const sourceNodeName = document.getElementById("sourceNode").value;
+    const targetNodeName = document.getElementById("targetNode").value;
 
-    if (node1 && node2) {
-        links.push({ node1, node2 });
-        drawNodes();
+    if (!sourceNodeName || !targetNodeName) {
+        alert("Please enter both source and target node names.");
+        return;
+    }
+
+    // Check if the source and target nodes exist
+    const sourceNode = nodes.find(node => node.name === sourceNodeName);
+    const targetNode = nodes.find(node => node.name === targetNodeName);
+
+    if (!sourceNode || !targetNode) {
+        alert("Source and/or target node does not exist.");
+        return;
+    }
+
+    const link = { source: sourceNode.name, target: targetNode.name };
+    links.push(link);
+    draw();
+}
+
+function deleteNode() {
+    if (selectedNode) {
+        // Remove the selected node and associated links
+        nodes.splice(nodes.indexOf(selectedNode), 1);
+        links.splice(links.findIndex(link => link.source === selectedNode.name || link.target === selectedNode.name), 1);
+        selectedNode = null;
+        draw();
     }
 }
 
-// Function to select/deselect nodes on canvas click
-function selectNode(e) {
+canvas.addEventListener("mousedown", function (e) {
     const mouseX = e.clientX - canvas.getBoundingClientRect().left;
     const mouseY = e.clientY - canvas.getBoundingClientRect().top;
 
@@ -67,52 +76,69 @@ function selectNode(e) {
                 // Select the clicked node
                 selectedNode = node;
             }
-            drawNodes();
             break;
         }
     }
-}
 
-// Function to delete the selected node
-function deleteSelectedNode() {
+    draw();
+});
+
+canvas.addEventListener("mousemove", function (e) {
     if (selectedNode) {
-        // Remove the selected node and associated links
-        const index = nodes.indexOf(selectedNode);
-        nodes.splice(index, 1);
-        links = links.filter(link => link.node1 !== selectedNode && link.node2 !== selectedNode);
-        selectedNode = null;
-        drawNodes();
+        selectedNode.x = e.clientX - canvas.getBoundingClientRect().left;
+        selectedNode.y = e.clientY - canvas.getBoundingClientRect().top;
+        draw();
+    }
+});
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw links
+    for (const link of links) {
+        const sourceNode = nodes.find(node => node.name === link.source);
+        const targetNode = nodes.find(node => node.name === link.target);
+        if (sourceNode && targetNode) {
+            // Calculate the angle between source and target nodes
+            const dx = targetNode.x - sourceNode.x;
+            const dy = targetNode.y - sourceNode.y;
+            const angle = Math.atan2(dy, dx);
+
+            // Draw the link line
+            ctx.beginPath();
+            ctx.moveTo(sourceNode.x, sourceNode.y);
+            ctx.lineTo(targetNode.x, targetNode.y);
+            ctx.strokeStyle = "black";  // Link color
+            ctx.stroke();
+
+            // Draw an arrowhead at the target node
+            const arrowSize = 10;
+            const arrowX = targetNode.x - 20 * Math.cos(angle);
+            const arrowY = targetNode.y - 20 * Math.sin(angle);
+            ctx.fillStyle = "red";  // Arrow color
+            ctx.beginPath();
+            ctx.moveTo(arrowX, arrowY);
+            ctx.lineTo(arrowX - arrowSize * Math.cos(angle - Math.PI / 6), arrowY - arrowSize * Math.sin(angle - Math.PI / 6));
+            ctx.lineTo(arrowX - arrowSize * Math.cos(angle + Math.PI / 6), arrowY - arrowSize * Math.sin(angle + Math.PI / 6));
+            ctx.closePath();
+            ctx.fill();
+        }
+    }
+
+    // Draw nodes
+    for (const node of nodes) {
+        ctx.beginPath();
+        ctx.arc(node.x, node.y, 20, 0, 2 * Math.PI);
+        ctx.fillStyle = selectedNode === node ? "red" : "blue";
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.fillStyle = "white";
+        ctx.font = "12px Arial";
+        ctx.textAlign = "center";  // Center the text horizontally
+        ctx.textBaseline = "middle";  // Center the text vertically
+        ctx.fillText(node.name, node.x, node.y);
     }
 }
 
-// Function to draw nodes and their connections on the canvas
-function drawNodes() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    nodes.forEach(node => {
-        ctx.beginPath();
-        ctx.arc(node.x, node.y, 20, 0, 2 * Math.PI);
-        ctx.fillStyle = selectedNode === node ? "red" : "#007bff";
-        ctx.fill();
-        ctx.closePath();
-
-        ctx.font = "12px Arial";
-        ctx.fillStyle = "#fff";
-        ctx.fillText(node.nodeName1, node.x - 15, node.y - 25);
-        ctx.fillText(node.nodeName2, node.x - 15, node.y + 15);
-    });
-
-    // Draw connections between nodes
-    ctx.strokeStyle = "#007bff";
-    ctx.lineWidth = 2;
-    links.forEach(link => {
-        ctx.beginPath();
-        ctx.moveTo(link.node1.x, link.node1.y);
-        ctx.lineTo(link.node2.x, link.node2.y);
-        ctx.stroke();
-        ctx.closePath();
-    });
-}
-
-// Initial canvas drawing
-drawNodes();
+draw();
---
+++ script.js
@@ -0,0 +1,119 @@
+// script.js
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// Array to store nodes and their positions
+const nodes = [];
+
+// Array to store links between nodes
+let links = [];
+
+// Variable to keep track of the selected node
+let selectedNode = null;
+
+// Event listener for the "Add Node" button
+document.getElementById("addNodeButton").addEventListener("click", addNode);
+
+// Event listener for the "Link Nodes" button
+document.getElementById("linkNodesButton").addEventListener("click", linkNodes);
+
+// Event listener for the "Delete Node" button
+document.getElementById("deleteNodeButton").addEventListener("click", deleteSelectedNode);
+
+// Event listener for canvas click to select/deselect nodes
+canvas.addEventListener("click", selectNode);
+
+// Function to add a new node
+function addNode() {
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+    const nodeName1 = document.getElementById("nodeName1").value;
+    const nodeName2 = document.getElementById("nodeName2").value;
+    
+
+    nodes.push({ x, y, nodeName1, nodeName2 });
+    drawNodes();
+}
+
+// Function to link nodes
+function linkNodes() {
+    const nodeName1 = document.getElementById("nodeName1").value;
+    const nodeName2 = document.getElementById("nodeName2").value;
+
+    const node1 = nodes.find(node => node.nodeName1 === nodeName1 || node.nodeName2 === nodeName1);
+    const node2 = nodes.find(node => node.nodeName1 === nodeName2 || node.nodeName2 === nodeName2);
+
+    if (node1 && node2) {
+        links.push({ node1, node2 });
+        drawNodes();
+    }
+}
+
+// Function to select/deselect nodes on canvas click
+function selectNode(e) {
+    const mouseX = e.clientX - canvas.getBoundingClientRect().left;
+    const mouseY = e.clientY - canvas.getBoundingClientRect().top;
+
+    // Check if a node is clicked
+    for (const node of nodes) {
+        const dx = node.x - mouseX;
+        const dy = node.y - mouseY;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < 20) {
+            if (selectedNode === node) {
+                // Toggle selection off
+                selectedNode = null;
+            } else {
+                // Select the clicked node
+                selectedNode = node;
+            }
+            drawNodes();
+            break;
+        }
+    }
+}
+
+// Function to delete the selected node
+function deleteSelectedNode() {
+    if (selectedNode) {
+        // Remove the selected node and associated links
+        const index = nodes.indexOf(selectedNode);
+        nodes.splice(index, 1);
+        links = links.filter(link => link.node1 !== selectedNode && link.node2 !== selectedNode);
+        selectedNode = null;
+        drawNodes();
+    }
+}
+
+// Function to draw nodes and their connections on the canvas
+function drawNodes() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    nodes.forEach(node => {
+        ctx.beginPath();
+        ctx.arc(node.x, node.y, 20, 0, 2 * Math.PI);
+        ctx.fillStyle = selectedNode === node ? "red" : "#007bff";
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.font = "12px Arial";
+        ctx.fillStyle = "#fff";
+        ctx.fillText(node.nodeName1, node.x - 15, node.y - 25);
+        ctx.fillText(node.nodeName2, node.x - 15, node.y + 15);
+    });
+
+    // Draw connections between nodes
+    ctx.strokeStyle = "#007bff";
+    ctx.lineWidth = 2;
+    links.forEach(link => {
+        ctx.beginPath();
+        ctx.moveTo(link.node1.x, link.node1.y);
+        ctx.lineTo(link.node2.x, link.node2.y);
+        ctx.stroke();
+        ctx.closePath();
+    });
+}
+
+// Initial canvas drawing
+drawNodes();
